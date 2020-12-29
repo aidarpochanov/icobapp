a94b6f4da4e931c016f7111126c3974c41592a1d
Pochanov Aidar <aidar.pochanov@gmail.com>
2020-12-29 12:35:35 +0000

    turning txt download off

--- icobapp/urls.py
+++ icobapp/urls.py
@@ -18,21 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 from rest_framework.authtoken.views import obtain_auth_token
-from django.http import HttpResponse
-from django.views.generic import View
-from django.views.generic.detail import SingleObjectMixin
 
-class TextFieldView(SingleObjectMixin, View):
-
-    def get(self, request, *args, **kwargs):
-        content = "93FC0841CE4F9FA5654829F8469A1B5051A339F58C2D4902017670FEA7730849\ncomodoca.com\nf9121db688f3e83"
-        response = HttpResponse(content, content_type='text/plain; charset=utf8')
-        response['Content-Disposition'] = 'attachment; filename="49E8AE83E3D85A8C03AFA06A3BADAFB6.txt"'
-        return response
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('mainapp/', include('mainapp.urls')),
     path('auth/', obtain_auth_token),
-    path('.well-known/pki-validation/49E8AE83E3D85A8C03AFA06A3BADAFB6.txt', TextFieldView.as_view())
+
 ]
